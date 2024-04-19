773a15c1cf2f21e4600b50fc70a614fff1701f7a
00JAS00 <julian.m.sosa.muchut@gmail.com>
2024-04-19 03:56:50 -0300

    practico 5 y 6 de guia 3

--- Guia-3/g03p05.py
+++ Guia-3/g03p05.py
@@ -1,1 +1,9 @@
 # 5- Guardar en una lista los números pares mayores que 0 y menores que 31. 
+
+pares=[]
+
+for i in range(10):
+    numero=int(input("Ingrese un numero: "))
+    if numero%2 == 0 and numero>0 and numero<31:
+        pares.append(numero)
+print ( "Numeros pares y Mayores que 0 y menores que 31: ",pares )
--- Guia-3/g03p06.py
+++ Guia-3/g03p06.py
@@ -1,1 +1,31 @@
 # 6- Ingresar nombres en una lista sin repetir, luego buscar un nombre y de encontrarlo decir en qué posición está.
+
+nombres=[]
+toggle=""
+
+print("ESTE PROGRAMA TIENE DOS PARTES UNA PARA LA INTRODUCCION DE NOMBRES Y OTRA PARA SU POSTERIOR BUSQUEDA\n")
+# Ingresar nombres en una lista sin repetir
+print("EN ESTA PARTE DEL PROGRAMA DEBE INGRESAR NOMBRES")
+while toggle != "off":
+    inputNombre=str(input("Ingrese un nombre: "))
+    
+    if not(inputNombre in nombres):
+        nombres.append(inputNombre)
+    toggle=str(input("para seguir 'on' para terminar 'off': "))
+    print    
+    if toggle=="off" and len(nombres)<3:
+        toggle="on"
+        print("debe ingresar un minimo de 3 nombres para poder cerrar el programa \n")
+
+# luego buscar un nombre y de encontrarlo decir en qué posición está.
+print("AHORA DEBERA BUSCAR UN NOMBRE PARA SABER SI SE ENCUENTRA\n")
+inputNombre=str(input("Ingrese el nombre a buscar: "))
+posicionNombre=""
+for i in range(len(nombres)):
+    if nombres[i]==inputNombre:
+        posicionNombre=i
+        print("Nombre encontrado en la posicion: ",i)
+        break
+if posicionNombre=="":
+    print("El nombre no ha sido encontrado")
+    
