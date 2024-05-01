0cfb08da5384aa872d676218c8484b3534d9f15f
00JAS00 <julian.m.sosa.muchut@gmail.com>
2024-04-30 21:03:14 -0300

    ejercicio de g03 practico 11 y 12

--- Guia-3/g03p11.py
+++ Guia-3/g03p11.py
@@ -1,1 +1,24 @@
 # 11- Cargar en listas los nombres y fechas de nacimiento de varias personas, luego recorrerlo y mostrar los nombres de los mayores de edad.
+
+nombres = ["Juan", "María", "Pedro", "Ana"]
+fechasNacimiento = ["2000-05-15", "1998-10-20", "2005-03-10", "1985-12-31"]
+
+# Fecha actual
+añoActual = 2024
+mesActual = 4
+diaActual = 30
+
+
+for i in range(len(nombres)):
+    # Extraer el año, mes y día de la fecha de nacimiento
+    fechaNacimiento = fechasNacimiento[i].split("-")
+    anioNacimiento = int(fechaNacimiento[0])
+    mesNacimiento = int(fechaNacimiento[1])
+    diaNacimiento = int(fechaNacimiento[2])
+    
+    edad = añoActual - anioNacimiento
+
+    if (mesActual, diaActual) < (mesNacimiento, diaNacimiento):
+        edad -= 1
+    if edad >= 18:
+        print(nombres[i])
--- Guia-3/g03p12.py
+++ Guia-3/g03p12.py
@@ -1,1 +1,22 @@
 # 12- Pedir nombres y sexo de personas y mostrar al final el total de mujeres y el nombre de cada una.
+
+personas = []
+total_mujeres = 0
+
+while True:
+    nombre = input("Ingrese el nombre de la persona (o 'fin' para terminar): ")
+    if nombre == 'fin':
+        break
+    
+    sexo = input("Ingrese el sexo de la persona (M/F): ")
+    
+    if sexo in "Ff":
+        total_mujeres += 1
+        personas.append((nombre, sexo))
+
+# Mostrar el total de mujeres y sus nombres
+print("Total de mujeres:", total_mujeres)
+print("Nombres de las mujeres:")
+for persona in personas:
+    if persona[1] in "Ff":
+        print(persona[0])
